ca34f41fc8453b138f089e0ae6aae33241f7c9c0
Mathew Thomas <mathew.thomas@longdivision.com>
2018-07-10 21:03:17 -0400

    fixed port
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -p 8080:8080 --net=host  -v "$(pwd)"/public:/usr/src/app/public   -v "$(pwd)"/src:/usr/src/app/src  mthomas/node-web-3
+docker run -p 8585:8585 --net=host  -v "$(pwd)"/public:/usr/src/app/public   -v "$(pwd)"/src:/usr/src/app/src  mthomas/node-web-3
