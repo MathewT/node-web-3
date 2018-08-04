2427489fc98835be3ac74275f49b2db8c9c6d654
Mathew Thomas <mathew.thomas@longdivision.com>
2018-08-04 18:14:56 -0400

    fixed run.sh, added env.list
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-docker run -p 8585:8585 --net=host  -v "$(pwd)"/public:/usr/src/app/public   -v "$(pwd)"/src:/usr/src/app/src  mthomas/node-web-3
+## docker run -p 8585:8585 --net=host  -v "$(pwd)"/public:/usr/src/app/public   -v "$(pwd)"/src:/usr/src/app/src  mthomas/node-web-3
+
+docker run -d -p 8585:8585 --rm \
+  --env-file ./env.list \
+  --name "web-3" \
+  -v "$(pwd)"/public:/usr/src/app/public \
+  -v "$(pwd)"/src:/usr/src/app/src  mthomas/node-web-3
